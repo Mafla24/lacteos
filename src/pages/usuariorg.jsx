3683dc8f88f1055e702bfd4104c401b2ec8b66db
josemua <chepemual@gmail.com>
2021-10-31 00:50:20 -0500

    registro, login, header, home, rutas app Updated

--- src/pages/usuariorg.jsx
+++ src/pages/usuariorg.jsx
@@ -1,76 +1,111 @@
-import React, { useState } from 'react'
-import { createUser } from '../services/UsersService'
-import { useHistory } from 'react-router';
+import React, { useState } from "react";
+import { createUser } from "../services/UsersService";
+import { useHistory } from "react-router-dom";
+import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  container: {
+    width: "300px",
+    padding: "4%",
+    margin: "100px auto 0 auto",
+  },
+});
 
 const initialValue = {
-    email: '',
-    password: '', 
-    isAdmin: '', 
-    status: '', 
-    name: '', 
-    tel: ''
-}
+  email: "",
+  password: "",
+  name: "",
+  tel: "",
+};
 
-function Usuariorg(){
-    const [newUser, setNewUser] = useState(initialValue);
-    const {email, password, isAdmin, status, name, tel} = newUser;
+function Usuariorg() {
+  const [newUser, setNewUser] = useState(initialValue);
+  const { email, password, name, tel } = newUser;
 
-    let history = useHistory();
+  const classes = useStyles();
 
-    const onValueChange = (e) => {
-        setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  let history = useHistory();
+
+  const onValueChange = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  const registerUser = async () => {
+    let response = await createUser(newUser);
+    if (response.status === 201) {
+      history.push("/");
+    } else {
+      console.error("Error creando el usuario" + response.data.error);
     }
+  };
 
-    return (
-        <div>
-            
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
-    
-
-    <div>
-        <h1>Formulario de registro</h1>
-    </div>
-    <form>
-        <div class="form-group"> 
-            <label for="full_name_id" class="control-label"><b>Nombre</b></label>
-            <input type="text" value={name} onChange={(e) => onValueChange(e)} class="form-control" id="full_name_id" placeholder="Ingresa tu nombre"/>
-        </div>  
-        <div class="form-group"> 
-            <label for="full_name_id" class="control-label"><b>Telefono</b></label>
-            <input type="text" class="form-control" id="full_tele_id" placeholder="Ingresa numero telefonico"/>
-        </div>  
-        <div class="form-group"> 
-            <label for="full_name_id" class="control-label"><b>Correo</b></label>
-            <input type="email" class="form-control" id="full_email_id" placeholder="Ejemplo: correo@mail.com"/>
-        </div> 
-        <div class="form-group"> 
-            <label for="state_id" class="control-label"><b>Rol</b></label>
-            <select class="form-control" id="state_id">
-                <option value="AL">Vendedor</option>
-                <option value="AL">Administrador</option>
-            </select>                    
-        </div>
-        <div class="form-group"> 
-            <label for="state_id" class="control-label"><b>Estado</b></label>
-            <select class="form-control" id="state_id">
-                <option value="AL">Pendiente</option>
-                <option value="AL">Autorizado</option>
-                <option value="AL">No autorizado</option>
-            </select>                    
-        </div>
-
-        <div class="2"> 
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </div> 
-
-
-    </form>
-    
-
-
-</div>
-
-    );
+  return (
+    <Paper className={classes.container}>
+      <Typography variant="h4">Formulario de registro</Typography>
+      <Grid container spacing={8} error alignItems="flex-end">
+        <Grid item md={true} sm={true} xs={true}>
+          <TextField
+            value={name}
+            name="name"
+            onChange={(e) => onValueChange(e)}
+            label="Nombre Completo"
+            type="text"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={8} alignItems="flex-end">
+        <Grid item md={true} sm={true} xs={true}>
+          <TextField
+            value={tel}
+            name="tel"
+            onChange={(e) => onValueChange(e)}
+            label="Teléfono"
+            type="text"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={8} alignItems="flex-end">
+        <Grid item md={true} sm={true} xs={true}>
+          <TextField
+            value={email}
+            name="email"
+            onChange={(e) => onValueChange(e)}
+            label="Email"
+            type="email"
+            fullWidth
+            autoFocus
+            required
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={8} alignItems="flex-end">
+        <Grid item md={true} sm={true} xs={true}>
+          <TextField
+            value={password}
+            name="password"
+            onChange={(e) => onValueChange(e)}
+            label="Password"
+            type="password"
+            fullWidth
+            required
+          />
+        </Grid>
+      </Grid>
+      <Grid container justify="center" style={{ marginTop: "10px" }}>
+        <Button
+          variant="outlined"
+          onClick={() => registerUser()}
+          color="primary"
+          style={{ textTransform: "none" }}
+        >
+          Registrarse
+        </Button>
+      </Grid>
+    </Paper>
+  );
 }
 
 export default Usuariorg;
